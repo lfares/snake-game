466a25d7c5b77dfb85d76b7937e817309a0a4675
lfares <livia.fares97@gmail.com>
2020-05-04 14:50:42 -0300

    Added initiate game function with snake movements

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ snake[0] = {
     y: 8 * box
 } // put snake at the middle
 
+let direction = "right"
+
 function createBackGround() {
     context.fillStyle = "lightblue"
     context.fillRect(0, 0, 16*box, 16*box)
@@ -19,5 +21,27 @@ function createSnake() {
     }
 }
 
-createBackGround()
-createSnake()
+function initiateGame() {
+    createBackGround()
+    createSnake()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if (direction == "right") snakeX += box
+    else if (direction == "left") snakeX -= box
+    else if (direction == "up") snakeY -= box
+    else snakeY += box
+
+    snake.pop()
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead)
+}
+
+let game = setInterval(initiateGame, 100) // "restart" game to make sure it doesnt lag
+
+
+
