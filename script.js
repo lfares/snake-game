8459ededebba360defaf06e75e6f9c29546800ef
lfares <livia.fares97@gmail.com>
2020-05-04 14:39:31 -0300

    Added function to create snake

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake")
 let context = canvas.getContext("2d")
 let box = 32
+let snake = []
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+} // put snake at the middle
 
 function createBackGround() {
     context.fillStyle = "lightblue"
     context.fillRect(0, 0, 16*box, 16*box)
 }
 
-createBackGround();
+function createSnake() {
+    for (i = 0; i < snake.length; i++){
+        context.fillStyle = "blue"
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
+createBackGround()
+createSnake()
